78648962a6ccb58dc8c8b3d6849fa8afd64a0948
Ubuntu <ubuntu@ip-172-31-30-234.eu-west-2.compute.internal>
2020-11-15 13:19:53 +0000

    el b v2

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from flask import Flask
-app = Flask(__name__)
+application = Flask(__name__)
 
 @app.route('/')
 @app.route('/hello/')
@@ -12,4 +12,5 @@ def hello_user(username):
     return 'Why Hello %s!\n' % username
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')     # open for everyone
+    application.debug = True
+    application.run(host='0.0.0.0')     # open for everyone
